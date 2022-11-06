7d4b151222c7d9dce2143baea607c4a31a82efef
Andrew Lee <andrewjkes@hotmail.com>
2022-11-06 15:36:34 +0800

    Update config and improve chunck sizes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +14,25 @@ export default defineConfig({
         'src/**/*.js', 'src/**/*.jsx', 'src/*.jsx', 'src/*.js',
       ],
     }),
+    chunkSplitPlugin({
+      customSplitting: {
+        // `react` and `react-dom` will be bundled together in the `react-vendor` chunk (with their dependencies, such as object-assign)
+        'react-vendor': ['react', 'react-dom'],
+        utils: [/src\/utils/, /src\/constant/],
+        'firebase-vendor': ['firebase/app', 'firebase/auth', 'firebase/analytics', 'firebase/app-check'],
+        firestore: ['firebase/firestore'],
+        'framer-motion': ['framer-motion'],
+      }
+    }),
+    visualizer(),
+    nodeResolve()
   ],
   server: {
     watch: {
       usePolling: true,
     },
   },
+  build: {
+    format: 'iife'
+  }
 });
